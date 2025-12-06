b383e8bb37fe75f8bf7a8ab663b82a6529bd5d59
js <jamiesmith.exe@gmail.com>
2025-12-06 23:34:20 +0000

    ros2 configs added

--- comms/ROS_comms/example.py
+++ comms/ROS_comms/example.py
@@ -3,4 +3,4 @@ import random
 with ROS("temp_pub") as ros:
     temp = ros.publishers.Float32("/temperature")
     ros.timer(0.5, lambda: temp(random.uniform(25.0, 30.0)))
-    ros.run(30.0)
+    ros.run(3000000000.0)
