0d482fb48ddfbb54cb10d5471fa908730bec1440
jsmith-exe <jamiesmith.exe@gmail.com>
2025-12-10 01:23:29 +0000

    ros publisher architecture started

--- comms/ROS_comms/example.py
+++ comms/ROS_comms/example.py
@@ -1,6 +1,4 @@
-from publisher import ROS
-import random
-with ROS("temp_pub") as ros:
-    temp = ros.publishers.Float32("/temperature")
-    ros.timer(0.5, lambda: temp(random.uniform(25.0, 30.0)))
-    ros.run(3000000000.0)
+from temp_publisher import TemperaturePublisher
+
+if __name__ == "__main__":
+    TemperaturePublisher.run()
