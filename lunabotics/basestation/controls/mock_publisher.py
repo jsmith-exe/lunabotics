074c1f26dc7f1bccf6fa4b82913450329b2e9024
caleb <calebthair@outlook.com>
2025-12-30 01:32:23 +0000

    Updated mocks to work with fully supported controller

--- lunabotics/basestation/controls/mock_publisher.py
+++ lunabotics/basestation/controls/mock_publisher.py
@@ -19,9 +19,8 @@ class TurtleRover:
         self.screen.tracer(0)
         TurtleRover.instance = self
 
-        self.command_state = dict()
-        for command in Commands:
-            self.command_state[command] = False
+        self.thrust = 0
+        self.turning_thrust = 0
 
         # Set up update loop
         def check_commands():
@@ -36,20 +35,10 @@ class TurtleRover:
     def process_state(self):
         """ Given the stored state, applies movement to the turtle. """
         speed = 5
-        if self.command_state[Commands.FORWARD]:
-            self.turtle.forward(speed)
-        if self.command_state[Commands.LEFT]:
-            self.turtle.left(speed)
-        if self.command_state[Commands.RIGHT]:
-            self.turtle.right(speed)
-        if self.command_state[Commands.REVERSE]:
-            self.turtle.backward(speed)
-
+        turning_speed = 5
+        self.turtle.forward(self.thrust * speed)
+        self.turtle.right(self.turning_thrust * turning_speed)
         self.screen.update()
-
-    def update_command_state(self, command: str, state: bool):
-        """ Updates some state of the command. """
-        self.command_state[command] = state
 
 def mock_publish(command: str, *args):
     """
@@ -57,16 +46,37 @@ def mock_publish(command: str, *args):
     :param command: an initial command.
     :param args: arbitrary list of arguments.
     """
+    print(args)
     rover = TurtleRover.instance
     if rover is None:
         return
 
-    if command == Commands.STOP_SIGNAL:
-        command_to_stop = args[0]
-        rover.update_command_state(command_to_stop, False)
-        return
+    default = 1
 
-    rover.update_command_state(command, True)
+    thrust = None
+    turning_thrust = None
+    match command:
+        case Commands.FORWARD:
+            thrust = get_index(args, 0, default)
+        case Commands.REVERSE:
+            thrust = -get_index(args, 0, default)
+        case Commands.LEFT:
+            turning_thrust = -get_index(args, 0, default)
+        case Commands.RIGHT:
+            turning_thrust = get_index(args, 0, default)
+        case Commands.STOP_SIGNAL:
+            command = args[0]
+            if command == Commands.FORWARD or command == Commands.REVERSE: thrust = 0
+            else: turning_thrust = 0
+
+    if thrust is not None: rover.thrust = thrust
+    if turning_thrust is not None: rover.turning_thrust = turning_thrust
+
+def get_index(args, index, default):
+    try:
+        return args[index]
+    except IndexError:
+        return default
 
 if __name__ == "__main__":
     state = BaseStationState()
