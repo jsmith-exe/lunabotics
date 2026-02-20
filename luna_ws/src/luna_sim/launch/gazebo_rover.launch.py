4bc300237d96e210b889b3532469f06cc6156bf4
js <jamiesmith.exe@gmail.com>
2026-02-20 17:38:31 +0000

    updated slam config

--- luna_ws/src/luna_sim/launch/gazebo_rover.launch.py
+++ luna_ws/src/luna_sim/launch/gazebo_rover.launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
     world_path = os.path.join(
         get_package_share_directory(package_name),
         "worlds",
-        "arena.world"
+        "cones.world"
     )
 
     rsp = IncludeLaunchDescription(
--- process/startup.sh
+++ process/startup.sh
@@ -13,6 +13,7 @@ alias luna_packages='${LUNA_PROJECT}/process/install_packages.sh'
 alias luna_sim_rviz="ros2 launch luna_sim gazebo_with_rviz.launch.py"
 alias luna_rviz_sim="luna_sim_rviz"
 alias luna_kb="ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r cmd_vel:=/cmd_vel"
+alias luna_gaz="ros2 launch luna_sim gazebo_rover.launch.py"
 
 # Make scripts executable
 chmod +x "$LUNA_PROJECT"/process/install_packages.sh
