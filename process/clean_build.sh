6c0f05ba2a24dfc7adf3b1794f594e7918767e22
Jamie Smith <jamiesmith.exe@gmail.com>
2026-03-15 18:30:37 +0000

    Source ROS setup in clean_build.sh
    Add source command for ROS setup in clean_build.sh

--- process/clean_build.sh
+++ process/clean_build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+source /opt/ros/humble/setup.bash
+
 cd "$QPL_PROJECT/qpl_ws" || {
   echo "Could not find qpl_ws. Check QPL_PROJECT is set correctly."
   exit 1
@@ -8,5 +10,4 @@ cd "$QPL_PROJECT/qpl_ws" || {
 
 rm -rf build/ install/ log/
 colcon build --symlink-install
-source /opt/ros/humble/setup.bash
 source install/setup.bash
