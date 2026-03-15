028dcb31d1cd566966e2137e12bc30546477ce3b
jsmith.exe <jamiesmith.exe@gmail.com>
2026-03-15 23:18:48 +0000

    nav lauch alias

--- process/startup.sh
+++ process/startup.sh
@@ -39,7 +39,7 @@ alias qpl_packages='${QPL_PROJECT}/process/install_packages.sh'
 alias qpl_kb='ros2 run teleop_twist_keyboard teleop_twist_keyboard --ros-args -r cmd_vel:=cmd_vel_teleop'
 alias qpl_slam='ros2 launch qpl_rover online_async_launch.py'
 alias qpl_cloud_to_scan='ros2 launch qpl_rover point_cloud_to_scan.launch.py'
-alias qpl_nav2='ros2 launch qpl_rover navigation_launch.py'
+alias qpl_nav='ros2 launch qpl_rover navigation_launch.py'
 
 # Use functions (not aliases) for anything that needs env switching
 qpl_headless() {
