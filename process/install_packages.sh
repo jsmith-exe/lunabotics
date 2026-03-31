96acea933c6e38b1e727eaed1238b9fff02c45a3
jsmith-exe <jamiesmith.exe@gmail.com>
2026-03-31 22:35:26 +0000

    updated nav2

--- process/install_packages.sh
+++ process/install_packages.sh
@@ -26,10 +26,20 @@ sudo apt -y install ros-humble-xacro \
   ros-humble-v4l2-camera \
   v4l-utils \
   ros-humble-rtabmap-ros
-  gazebo \
-  ros-humble-gazebo-ros-pkgs \
+  ros-humble-gazebo-ros-pkgs
+#   gazebo \
 
 sudo apt -y install mesa-utils
 
 sudo apt -y install libserial-dev
-  
+
+# For camera
+sudo apt install libgflags-dev nlohmann-json3-dev  \
+  ros-$ROS_DISTRO-image-transport  ros-${ROS_DISTRO}-image-transport-plugins ros-${ROS_DISTRO}-compressed-image-transport \
+  ros-$ROS_DISTRO-image-publisher ros-$ROS_DISTRO-camera-info-manager \
+  ros-$ROS_DISTRO-diagnostic-updater ros-$ROS_DISTRO-diagnostic-msgs ros-$ROS_DISTRO-statistics-msgs \
+  ros-$ROS_DISTRO-backward-ros libdw-dev
+# Additional camera setup
+cd ${QPL_PROJECT}/qpl_ws/src/OrbbecSDK_ROS2/orbbec_camera/scripts
+sudo bash install_udev_rules.sh
+sudo udevadm control --reload-rules && sudo udevadm trigger
--- qpl_ws/src/qpl_rover/nodes/front_camera.py
+++ qpl_ws/src/qpl_rover/nodes/front_camera.py
@@ -1,6 +1,5 @@
 import cv2
 
-cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
 if vc.isOpened(): # try to get the first frame
