2f2fbf6b81aad0a8c6377634d5d61ae42acba9f7
jsmith-exe <jamiesmith.exe@gmail.com>
2026-05-02 18:48:31 +0100

    Tweaked camera setup

--- process/functions/orbbec_sdk.sh
+++ process/functions/orbbec_sdk.sh
@@ -10,11 +10,16 @@ qpl_orbbecsdk_build() {
     ros-humble-image-transport  ros-humble-image-transport-plugins ros-humble-compressed-image-transport \
     ros-humble-image-publisher ros-humble-camera-info-manager \
     ros-humble-diagnostic-updater ros-humble-diagnostic-msgs ros-humble-statistics-msgs \
-    ros-humble-backward-ros libdw-dev
-  # Additional camera setup
-  cd "${QPL_PROJECT}/qpl_ws/src/OrbbecSDK_ROS2/orbbec_camera/scripts" || {
-    echo "Failed to change directory to ${QPL_PROJECT}/qpl_ws/src/OrbbecSDK_ROS2/orbbec_camera/scripts"
-    exit 1
+    ros-humble-backward-ros libdw-dev \
+    ros-humble-orbbec-camera ros-humble-orbbec-description
+  
+  # Registration script
+  sudo cp /opt/ros/$ROS_DISTRO/share/orbbec_camera/udev/99-obsensor-libusb.rules /etc/udev/rules.d/
+  sudo udevadm control --reload-rules && sudo udevadm trigger
+
+  cd "${QPL_PROJECT}/OrbbecSDK_ROS2/orbbec_camera/scripts" || {
+    echo "Failed to change directory to ${QPL_PROJECT}/OrbbecSDK_ROS2/orbbec_camera/scripts"
+    return
   }
   sudo bash install_udev_rules.sh
   sudo udevadm control --reload-rules && sudo udevadm trigger
@@ -22,10 +27,11 @@ qpl_orbbecsdk_build() {
   local prev_path=$(pwd)
   cd "${QPL_PROJECT}/OrbbecSDK_ROS2" || {
     echo "Failed to change directory to ${QPL_PROJECT}/OrbbecSDK_ROS2"
-    exit 1
+    return
   }
   colcon build --event-handlers  console_direct+  --cmake-args  -DCMAKE_BUILD_TYPE=Release
   cd "$prev_path" || true
+  source "${QPL_PROJECT}/OrbbecSDK_ROS2/install/setup.bash"
 }
 
 qpl_orbbecsdk_run() {
