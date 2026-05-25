a5ab61c464fca6ac018f53f3ba662c61161e9522
caleb <calebthair@outlook.com>
2026-05-25 21:54:55 +0100

    Tweaks to realsense launch

--- qpl_ws/src/qpl_rover/launch/hardware/camera_realsense.launch.py
+++ qpl_ws/src/qpl_rover/launch/hardware/camera_realsense.launch.py
@@ -8,15 +8,39 @@ def generate_launch_description():
         "camera.color.image_raw.ffmpeg.qmax": 51,
         "pointcloud__neon_.enable": True,
         "initial_reset": True,
+        'enable_gyro': True,
+        'enable_accel': True,
+        'unite_imu_method': 2,
+        # 'color_profile': '1280x720x30'
     }
+    camera_node = Node(
+        package='realsense2_camera',
+        executable='realsense2_camera_node',
+        name='camera',
+        namespace='camera',
+        parameters=[camera_params],
+        output='screen',
+    )
+
+    imu_filter = Node(
+        package='imu_filter_madgwick',
+        executable='imu_filter_madgwick_node',
+        name='imu_filter',
+        parameters=[{
+            'use_mag': False,
+            'publish_tf': False,
+            'world_frame': 'enu',
+            'gain': 0.1,
+            'zeta': 0.0,
+            'fixed_frame': 'camera_frame',
+            'remove_gravity_vector': False,
+        }],
+        remappings=[
+            ('/imu/data_raw', '/camera/camera/imu'),
+        ]
+    )
 
     return LaunchDescription([
-        Node(
-            package='realsense2_camera',
-            executable='realsense2_camera_node',
-            name='camera',
-            namespace='camera',
-            parameters=[camera_params],
-            output='screen',
-        )
+        camera_node,
+        imu_filter,
     ])
