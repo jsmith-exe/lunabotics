e53666e1477c69efc8371f1ccc1fecead8f81754
caleb <calebthair@outlook.com>
2026-07-19 18:21:20 +0100

    Updated can sim launch to resolve permission issues

--- qpl_ws/src/can_sim/launch/can_sim.launch.py
+++ qpl_ws/src/can_sim/launch/can_sim.launch.py
@@ -1,16 +1,34 @@
+import os
+
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, DeclareLaunchArgument
+from launch.actions import ExecuteProcess, DeclareLaunchArgument, TimerAction
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 
+FAKE_USB = "/tmp/fake_can_rx"
+CAN_USB = "/dev/ttyUSB0"
+
+def delay(process, period):
+    return TimerAction(actions=[process], period=period)
 
 def generate_launch_description():
-    example_command = ExecuteProcess(
-        cmd=["sudo", "socat", "PTY,link=/dev/ttyUSB0,rawer", "PTY,link=/tmp/fake_can_rx,rawer"],
+    # Create fake USB
+    socat_process = ExecuteProcess(
+        cmd=["sudo", "socat", f"PTY,link={CAN_USB},rawer", f"PTY,link={FAKE_USB},rawer"],
         output="screen",
     )
 
-    example_node = Node(
+    # Grants fake USB read write perms, regardless of user
+    chmod_fake_usb_process = ExecuteProcess(
+        cmd=["sudo", "chmod", "666", FAKE_USB],
+        output="screen",
+    )
+    chmod_can_usb_process = ExecuteProcess(
+        cmd=["sudo", "chmod", "666", CAN_USB],
+        output="screen",
+    )
+
+    can_sim_node = Node(
         package="can_sim",
         executable="can_sim",
         name="can_sim",
@@ -18,6 +36,8 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        example_command,
-        example_node,
+        socat_process,
+        delay(chmod_fake_usb_process, 1.0),
+        delay(chmod_can_usb_process, 1.0),
+        delay(can_sim_node, 2.0),
     ])
