1aa4caf32779e70bdc6b8b671e3410c2395bc940
Katrina <katrina5566@icloud.com>
2026-08-16 20:30:16 +0100

    apriltag refactor: set up apriltag location in config

--- qpl_ws/src/qpl_rover/qpl_rover/apriltag_observer.py
+++ qpl_ws/src/qpl_rover/qpl_rover/apriltag_observer.py
@@ -73,8 +73,8 @@ class AprilTagObserver(Node):
 
         # 3. CALCULATE STATIC TRANSFORMS
         # Tag pose in MAP frame
-        tag_pos = [0.0, 0.2, 0.2]
-        tag_q = tf_transformations.quaternion_from_euler(0, 0, np.pi)
+        tag_pos = arena_config['apriltag']['position']
+        tag_q = arena_config['apriltag']['quaternion']
         self.T_map_tag = self.make_tf_matrix(tag_pos, tag_q)
 
         # TF listener for rover/camera transforms
